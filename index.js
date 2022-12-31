1e8c45d359a2fbc864f50d376027dbf5edf9c18d
rajachary <rajatherajachary@gmail.com>
2022-12-31 08:22:23 +0000

    basics

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hello NxtWave</h1>
+    <p className="paragraph">Good To Learn Here</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
